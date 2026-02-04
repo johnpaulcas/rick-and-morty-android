4a5c8b43fa824153a4cbaace1799f42381e7f3e3
johnpaulcas <casjp.app@gmail.com>
2026-02-04 21:48:45 +0800

    🎉 init

---
+++ app/src/main/java/com/johnpaulcas/rickandmorty/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.johnpaulcas.rickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.johnpaulcas.rickandmorty.ui.theme.RickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RickAndMortyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    App(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello from Rick and Morty"
+    )
+}
+
+@Composable
+@Preview
+fun AppPreview() {
+    RickAndMortyTheme {
+        App()
+    }
+}
