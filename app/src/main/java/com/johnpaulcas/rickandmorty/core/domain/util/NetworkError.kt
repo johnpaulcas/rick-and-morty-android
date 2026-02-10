468d51af798f6be8974b8783d34eef794e048345
johnpaulcas <casjp.app@gmail.com>
2026-02-10 23:37:14 +0800

    ✨ add network error enum class

--- app/src/main/java/com/johnpaulcas/rickandmorty/core/domain/util/NetworkError.kt
+++ app/src/main/java/com/johnpaulcas/rickandmorty/core/domain/util/NetworkError.kt
@@ -1,5 +1,8 @@
 package com.johnpaulcas.rickandmorty.core.domain.util
 
+import android.content.Context
+import com.johnpaulcas.rickandmorty.R
+
 enum class NetworkError: Error {
     REQUEST_TIMEOUT,
     TOO_MANY_REQUESTS,
